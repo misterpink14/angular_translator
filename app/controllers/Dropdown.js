99dafb1be40f8615ca55327689819c67373048d3
misterpink14 <misterpink14@gmail.com>
2015-10-08 18:47:15 +0000

    finished basic implementation

--- app/controllers/Dropdown.js
+++ app/controllers/Dropdown.js
@@ -18,5 +18,24 @@ app.controller("Dropdown", function($scope) {
 		name: "Russian",
 		value: "ru"
 	}];
-	$scope.colour = "";
+	$scope.t = function() {
+		var text = document.getElementById("english").value
+		var elm = document.getElementById("language")
+		var lang =  elm.options[elm.selectedIndex].text
+	
+		var url = "https://api.mymemory.translated.net/get?q="+ encodeURIComponent(text.trim()) +"&langpair=en|" + lang.trim();
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", url, false);
+		xhr.send();
+		var trans = JSON.parse(xhr.responseText);
+	
+		console.log(trans)
+		if (trans.responseStatus != 200)
+		{
+			alert("ERROR: Please enter text into the first box")
+			return
+		}
+		document.getElementById("translation").value = trans.responseData.translatedText;
+	
+	}
 });
